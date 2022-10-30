58c099049e29409366a3263a433ad093dca88c21
Damustin <damianamlp@hotmail.com>
2022-10-29 23:17:18 -0300

    entrega 5
    commits en product- info.js para guardar el producto en el LS y commits enb carrito.JS para mostrar los productos guardados

--- js/carrito.js
+++ js/carrito.js
@@ -1,1 +1,74 @@
-nameuserandbuton()
+
+let infousercart = CART_INFO_URL + "25801" + EXT_TYPE;
+let carritoactual = [];
+const listener = document.getElementById("container-carrito");
+
+ listener.addEventListener("input",function(e){
+    modificarcant(e.target.id,e.target.value)
+    
+ }
+ )
+function modificarcant(id,cantidad){
+   carritoactual.forEach(element => {
+    if (element.id == id){ element.cantidad = cantidad}
+   })
+ 
+   mostrarcarrito(carritoactual)
+
+
+}
+
+
+function mostrarcarrito(producto) {
+   
+    let newart = "";
+  
+    for (let index = 0; index < producto.length; index++) {
+        const element = producto[index];
+        
+
+      
+       
+   
+     newart += `<div class="row">
+<div class="col">
+<img src="${element.images[0]}" alt="imgprod" width="180" height="100"> 
+</div>
+<div class="col">
+${element.name}
+</div>
+<div class="col">
+${element.currency + element.cost}
+</div>
+<div class="col">
+<input  type="number" value="${element.cantidad}"
+min="1" max="10" id=${element.id} 
+>
+</div>
+<div class="col" >
+${(element.currency)+(element.cantidad)*(element.cost)}
+</div>
+</div>`}
+
+
+        document.getElementById("container-carrito").innerHTML = newart +"<hr>"
+    }
+    
+
+
+document.addEventListener("DOMContentLoaded", function () {
+        if (localStorage.getItem("carrito")) {
+             carritoactual = JSON.parse(localStorage.getItem("carrito"))
+             console.log(carritoactual)
+             console.log(infousercart)
+             mostrarcarrito(carritoactual)
+        
+        }
+      
+        nameuserandbuton();
+        
+    })
+
+
+
+
--- js/product-info.js
+++ js/product-info.js
@@ -1,117 +1,151 @@
 
-let arrayparashowpro = PRODUCT_INFO_URL + localStorage.getItem("productID") + EXT_TYPE
-let array2 = []
-let linkcomentarios = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("productID") + EXT_TYPE
-let array3 = []
+let arrayparashowpro = PRODUCT_INFO_URL + localStorage.getItem("productID") + EXT_TYPE;
+let array2 = [];
+let linkcomentarios = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("productID") + EXT_TYPE;
+let array3 = [];
+let botoncomprar = "";
+let producto = "";
+let cart = [];
 
 function setproductID(id) {
-    localStorage.setItem("productID", id);
-    window.location = "product-info.html"
+  localStorage.setItem("productID", id);
+  window.location = "product-info.html"
 }
 
 
 
-
 function showcomentarios(array) {
-    let coment = "";
+  let coment = "";
 
-    for (let i = 0; i < array.length; i++) {
-        let newarray = array[i];
-        let scor = newarray.score
-        let starok= `<span class="fa fa-star checked"></span>`
-        let starnot= `<span class="fa fa-star"></span>`
+  for (let i = 0; i < array.length; i++) {
+    let newarray = array[i];
+    let scor = newarray.score
+    let starok = `<span class="fa fa-star checked"></span>`
+    let starnot = `<span class="fa fa-star"></span>`
 
-        coment += `<div class="col">
+    coment += `<div class="col">
       
     <h2 class="p4"> ${newarray.user}</h2><p class="mb-1">dice ${newarray.description}<br> fecha: ${newarray.dateTime} 
     puntuacion ${newarray.score}</p>
 </div>
 <div id"star"> 
 ${starok.repeat(scor)}
-${starnot.repeat(5-scor)}
+${starnot.repeat(5 - scor)}
 </div>
     `
-        document.getElementById("comentarios").innerHTML = coment;
-    
+    document.getElementById("comentarios").innerHTML = coment;
 
-    }
+
+  }
 }
 
 function showproducts(array) {
-    let newelement = `
-<div class="row">
+  producto = array.data
+  botoncomprar = array.data.id
+  let newelement = `
+    <div class="row">
     <div class="col">
         <div class="d-flex w-100 justify-content-between">
             <h1 class="mb-1">${array.data.name}</h1>
-            <small class="text-muted">${array.data.soldCount} artículos</small>
+            <p class="text-muted">${array.data.soldCount} artículos</p> 
+            <button id="${array.data.id}" type="button"
+             class="btn btn-success btn-lg ">Comprar <i class="fas fa-shopping-cart"></i></button>
+          
+        </div>
+
+        <p class="mb-1">${array.data.description}</p>
     </div>
-
-            <p class="mb-1">${array.data.description}</p>
-        </div>  
 </div>
-
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-<img src="${array.data.images[0]}" alt="imgn"  class="d-block w-100">
-</div>
-<div class="carousel-item">
-<img src="${array.data.images[1]}" alt="imgn1"  class="d-block w-100">
-</div>
-<div class="carousel-item">
-<img src="${array.data.images[2]}" alt="imgn2"  class="d-block w-100">
-</div>
-<div class="carousel-item">
-<img src="${array.data.images[3]}" alt="imgn3" class="d-block w-100">
-</div>
-</div>
-</div>
-<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-<span class="visually-hidden">Previous</span>
-</button>
-<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-<span class="carousel-control-next-icon" aria-hidden="true"></span>
-<span class="visually-hidden">Next</span>
-</button>
-</div>
+    <div class="carousel-inner" >
+      <div class="carousel-item active">
+        <img src="${array.data.images[0]}" alt="imgn" class="d-block w-100">
+      </div>
+      <div class="carousel-item">
+        <img src="${array.data.images[1]}" alt="imgn1" class="d-block w-100">
+      </div>
+      <div class="carousel-item">
+        <img src="${array.data.images[2]}" alt="imgn2" class="d-block w-100">
+      </div>
+      <div class="carousel-item">
+      <img src="${array.data.images[3]}" alt="imgn3" class="d-block w-100">
+  </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
 <div class="list-group-item list-group-item-action cursor-active">
 <h1>Productos relacionados<h1>
 </div>
-<div onclick="setproductID(${array.data.relatedProducts[0].id})"class="list-group-item list-group-item-action cursor-active">
+<div onclick="setproductID(${array.data.relatedProducts[0].id})class="prodrelat" class="list-group-item list-group-item-action cursor-active">
 <h1 class="mb-1">${array.data.relatedProducts[0].name}</h1>
 <img src="${array.data.relatedProducts[0].image}" alt="imgn" class="img-thumbnail">
 </div>
-<div onclick="setproductID(${array.data.relatedProducts[1].id})"class="list-group-item list-group-item-action cursor-active">
+<div onclick="setproductID(${array.data.relatedProducts[1].id})class="prodrelat" "class="list-group-item list-group-item-action cursor-active">
 <h1 class="mb-1">${array.data.relatedProducts[1].name}</h1>
 <img src="${array.data.relatedProducts[1].image}" alt="imgn" class="img-thumbnail">
 </div>      
         `
-    document.getElementById("aqui").innerHTML = newelement
+  document.getElementById("aqui").innerHTML = newelement;
 
-    console.log(typeof array3)
-    console.log(linkcomentarios)
-};
+  document.getElementById(botoncomprar).addEventListener("click", function () {
+    if (localStorage.getItem("carrito")) {
+      cart = JSON.parse(localStorage.getItem("carrito"))
+      
 
-console.log(arrayparashowpro)
+      if (cart.some(articulo => articulo.id == producto.id)) {
+        prompt("se agrego a tu carrito")
+        producto.cantidad ++
+       
+
+      }
+      else {
+     
+        cart.push(producto);
+        localStorage.setItem("carrito", JSON.stringify(cart))
+      }
+    }
+
+    else {
+     
+      cart.push(producto)
+      localStorage.setItem("carrito", JSON.stringify(cart))
+    }
+  })
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
-    getJSONData(arrayparashowpro).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            array2 = resultObj
-            showproducts(array2)
-        }
-    })
-
-
-    getJSONData(linkcomentarios).then(function (respon) {
-        if (respon.status === "ok") {
-            array3 = respon.data
-            showcomentarios(array3)
-            nameuserandbuton()
-        }
+  getJSONData(arrayparashowpro).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      array2 = resultObj
+      showproducts(array2)
     }
-    )
+  })
+
+
+  getJSONData(linkcomentarios).then(function (respon) {
+    if (respon.status === "ok") {
+      array3 = respon.data
+      showcomentarios(array3)
+      nameuserandbuton()
+
+    }
+  }
+  )
+
 
 })
+
+
+
+
+
 
